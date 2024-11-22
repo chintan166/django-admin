2f985efbd3a14392f9088d6f558d8eb6adbf0bd6
chintan166 <chintansuthar61@gmail.com>
2024-11-22 14:53:17 +0530

    second commit

--- custom_admin/core/admin.py
+++ custom_admin/core/admin.py
@@ -3,13 +3,26 @@ from .models import Course,Lesson
 
 admin.site.site_header = "chintan django custom header"
 
+class LessionInline(admin.TabularInline):
+    model = Lesson
+    max_num = 5
+    extra = 1
+    exclude = ('video_url',)
+    can_delete=True
+
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title','published_date','price','full_title')
-    list_display_links = ('price','published_date')
+    #list_display_links = ('price','published_date')
+    list_editable = ('published_date','price')
+    list_filter = ('title','published_date',)
+    search_fields = ('title',)
+    readonly_fields = ('status',)
+    inlines = (LessionInline,)
+    
     #fields = ('title','published_date','status','author')
     fieldsets=(
         (None,{
-          'fields':('title','published_date')  
+          'fields':('title','published_date','price')  
         }),
         ('Extra info',{
             'classes':('collapse','wide'),
@@ -21,7 +34,10 @@ class CourseAdmin(admin.ModelAdmin):
     
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-   pass
-    
+    list_display = ('title','course')
+    #list_filter = ('course__status')
+    search_fields = ('title',)
+    list_editable = ('course',)
+    autocomplete_fields = ('course',)
 
 admin.site.register(Course,CourseAdmin)
